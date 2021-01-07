194a586afa6dd87d16a3b2fee82717507dd0c170
aragon <andregaragon@gmail.com>
2021-01-07 11:10:02 -0300

    flask_restfull package added to project

---
+++ app/app/api/endpoints/registration.py
@@ -0,0 +1,17 @@
+from flask import request
+from ...core.database import users
+from ...main import app
+
+
+@app.route("/registration/", methods=['POST'])
+def route_registration():
+    # username = request.args['username']
+    # userspassword = request.args['userspassword']
+    # token = request.args['token']
+    # email = request.args['usersemail']
+    # address = request.args['usersaddress']
+    users_data = []
+    for user in users:
+        user_data = {"name": user.name, "email": user.email}
+        users_data.append(user_data)
+    return
