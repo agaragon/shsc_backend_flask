5ca37cfc3e675d7df0dea724bc73e74bb87697ff
aragon <andregaragon@gmail.com>
2021-01-09 07:13:30 -0300

    successfully creating and using DATABASE_URL both form production and local Dockerfile

--- app/app/main.py
+++ app/app/main.py
@@ -2,12 +2,3 @@ import os
 from app import app, db
 from .core import app_setup
 from .models import user
-
-# sam = user.User('Sam','1234','12345','Floripa','Sam@gmail.com')
-# Pedro = user.User('Pedro','1234','12345','Floripa','Pedro@gmail.com')
-
-# db.session.add_all([sam,Pedro])
-# db.session.commit()
-
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=os.environ['PORT'])
